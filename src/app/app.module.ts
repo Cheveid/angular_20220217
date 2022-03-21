e750f11871ab3f99b0ba8a389a034aabfa1dafdc
AKurbatov <AKurbatov83@mail.ru>
2022-03-21 11:27:20 +1100

    Домашнее задание 1
    1. Добавлен хедер (название магазина, иконка корзины).
    2. Создана карточка товара.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductTableComponent } from './components/product-table/product-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductCardComponent,
+    ProductTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+
+@Component({
+    selector: 'app-product-card',
+    templateUrl: './product-card.component.html',
+    styleUrls: ['./product-card.component.less']
+})
+export class ProductCardComponent implements OnInit {
+    @Input()
+    product: any = {};
+    
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
